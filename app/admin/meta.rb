e35a4e4d475d6508a9866da8719a48f68dc646a6
Prelang Builder <builder@prelang.com>
2016-11-21 16:59:23 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Meta' to allow for create, update

--- app/admin/meta.rb
+++ app/admin/meta.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Meta do
+  permit_params :data
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
