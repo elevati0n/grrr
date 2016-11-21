d42fa5bde7823cc03e79106a6f932ddb4b3a07f7
Prelang Builder <builder@prelang.com>
2016-11-21 16:59:32 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Approval' to allow for create, update

--- app/admin/approval.rb
+++ app/admin/approval.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Approval do
+  permit_params :approve
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
