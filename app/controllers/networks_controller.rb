50df55900eb3297cba25657cbca4d2c3e8d0db06
Prelang Builder <builder@prelang.com>
2016-11-21 16:59:15 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -3,7 +3,7 @@ class NetworksController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_network, only: [:show, :edit, :update, :destroy]
+  before_action :set_network, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /networks
   # GET /networks.json
