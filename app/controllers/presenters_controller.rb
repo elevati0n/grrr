ae5093518fb3ab5d43bd160b1317dd6ca02694f0
Prelang Builder <builder@prelang.com>
2016-11-21 17:00:24 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/presenters_controller.rb
+++ app/controllers/presenters_controller.rb
@@ -1,5 +1,5 @@
 class PresentersController < ApplicationController
-  before_action :set_presenter, only: [:show, :edit, :update, :destroy]
+  before_action :set_presenter, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /presenters
   # GET /presenters.json
