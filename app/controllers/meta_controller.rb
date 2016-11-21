2d1c1f78696b674f450dfedbefaef4f7632f659c
Prelang Builder <builder@prelang.com>
2016-11-21 16:59:23 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -3,7 +3,7 @@ class MetaController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_metum, only: [:show, :edit, :update, :destroy]
+  before_action :set_metum, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /meta
   # GET /meta.json
