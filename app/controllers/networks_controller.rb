fee290b4e2c200db08df83b0234342e609bea3be
Prelang Builder <builder@prelang.com>
2016-11-21 16:59:09 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -1,4 +1,8 @@
 class NetworksController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_network, only: [:show, :edit, :update, :destroy]
 
   # GET /networks
