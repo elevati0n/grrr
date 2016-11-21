f6f7ebb641fc746ba884b25a026544f8f9ec4821
Prelang Builder <builder@prelang.com>
2016-11-21 17:00:20 +0000

    Generated scaffolding for model 'Presenter' via Rails

---
+++ app/views/presenters/_presenter.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! presenter, :id, :user_id, :created_at, :updated_at
+json.url presenter_url(presenter, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :presenters
+
   resources :subscriptions do
   #->Prelang (voting/acts_as_votable)
   member do
