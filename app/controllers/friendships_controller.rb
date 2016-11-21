f9ed35247183ad38de59b4cfaf4042fce54f1d4b
Prelang Builder <builder@prelang.com>
2016-11-21 16:59:05 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,7 @@ class FriendshipsController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_friendship, only: [:show, :edit, :update, :destroy]
+  before_action :set_friendship, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /friendships
   # GET /friendships.json
