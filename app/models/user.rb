3f1ee68aa35ce76c39a15a32f61bc37cb0803e5d
Prelang Builder <builder@prelang.com>
2016-11-21 16:58:56 +0000

    Installed relation 'has_many' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_many :memberships
   has_many :presenters
   has_many :friendships
+  has_many :subscriptions
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
