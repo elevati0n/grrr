abb01f445c63518e0c18385470acdcb3bc9a8818
sean <s@a.c>
2016-12-02 04:43:16 -0500

    changes

--- app/views/components.rb
+++ app/views/components.rb
@@ -2,14 +2,14 @@
 require 'opal'
 require 'react/react-source'
 require 'hyper-react'
-if React::IsomorphicHelpers.on_opal_client?
-  require 'opal-jquery'
-  require 'browser'
-  require 'browser/interval'
-  require 'browser/delay'
-  # add any additional requires that can ONLY run on client here
-end
+# if React::IsomorphicHelpers.on_opal_client?
+#   require 'opal-jquery'
+#   require 'browser'
+#   require 'browser/interval'
+#   require 'browser/delay'
+#   # add any additional requires that can ONLY run on client here
+# end
 
 
 
-require_tree './components'
+# require_tree './components'
